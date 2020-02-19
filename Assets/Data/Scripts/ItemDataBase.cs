257d2873c6b66d59bb9f9ab7c931eb26449d54d5
WenRong <hybrid0930@gmail.com>
2020-02-19 23:51:53 +0800

    [add] ui scroller itembtn

---
+++ Assets/Data/Scripts/ItemDataBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDataBase : MonoBehaviour
+{
+    [SerializeField] private MOCK_DATA MOCK_DATA;
+    [SerializeField] private ScrollRect ItmeScroller;
+    [SerializeField] private GameObject ItemBtnPerfab;
+
+    private void Start()
+    {
+        foreach (var item in MOCK_DATA.dataArray)
+        {
+            InstantiateItemButton(item);
+        }
+    }
+
+    private void InstantiateItemButton(MOCK_DATAData data)
+    {
+        GameObject go = GameObject.Instantiate(ItemBtnPerfab, ItmeScroller.content);
+        ItemButton ib = go.GetComponent<ItemButton>();
+        ib.SetData(data);
+    }
+}
