708304ecf7e7c28273becc59eaaa87ae68e87ecd
WenRong <hybrid0930@gmail.com>
2020-02-24 00:20:23 +0800

    [add] Readme && hypelink content

--- Assets/Data/Scripts/HypeLink.cs
+++ Assets/Data/Scripts/HypeLink.cs
@@ -9,6 +9,7 @@ public class HypeLink : MonoBehaviour
 
     private void Start()
     {
+        LinkText.OnHrefClick.AddListener(HypesContent);
         var items = MOCK_DATA.dataArray
             .Select(i => new ItemData(i) { OnClick = OnItemBoxClick })
             .ToArray();
@@ -17,12 +18,22 @@ public class HypeLink : MonoBehaviour
 
     public void OnItemBoxClick(MOCK_DATAData data)
     {
-        LinkText.text += "Name: " + data.Content + ". Content: " + HrefContent(data) + ".\n";
+        LinkText.text += "Name: " + HrefName(data) + ".\n";
 
     }
 
-    private string HrefContent(MOCK_DATAData data)
+    private string HrefName(MOCK_DATAData data)
     {
-        return string.Format("<a href={0}>{1}</a>", data.Id, data.Content);
+        return string.Format("<a href={0}>{1}</a>", data.Id, data.Name);
+    }
+
+    private void HypesContent(string hypeId)
+    {
+        int id;
+        if (int.TryParse(hypeId, out id))
+        {
+            var data = MOCK_DATA.dataArray.FirstOrDefault(o => o.Id == id);
+            Debug.Log(string.Format("Name: <color=red>{0}</color>, Content: <color=blue>{1}</color>.", data.Name, data.Content));
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,26 @@
+# HyperlinksText #
+
+測試 [LinkImageText][LinkImageText] 應用。
+
+![Img_1]
+
+## Features ##
+
+使用 [QuickSheet][QuickSheet] 當資料庫，讓 [FancyScrollView][FancyScrollView] 顯示資料庫名字。
+
+點擊 FancyScrollView button 會在 [HypeLinkText][LinkImageText] 顯示 name。
+
+點擊 HypeLinkText 超連結文字，會顯示`超連結`內容。
+
+## repo ##
+
+* [LinkImageText][LinkImageText]
+* [QuickSheet][QuickSheet]
+* [FancyScrollView][FancyScrollView]
+
+________________________________________________________________________________
+
+[Img_1]:./doc/img/1.png
+[LinkImageText]:https://github.com/akof1314/uGUI_LinkImageText
+[QuickSheet]:https://github.com/kimsama/Unity-QuickSheet
+[FancyScrollView]:https://github.com/setchi/FancyScrollView
